a87efc969f651f46a0fa1d27d608676f60de059a
unknown <amoljadhav>
2023-04-09 21:12:51 +0530

    [Amol]Add:WelcomeToTheUserValidationProblem

---
+++ UserValidationProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using UserRegistrationProblem;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome To User Validation Program!");
+        Validation validation = new Validation();
+        validation.ValidateFirstName("Amol");
+        validation.ValidateFirstName("Ku");
+        validation.ValidateFirstName("Jai");
+        validation.ValidateFirstName("Sam");
+    }
+}
